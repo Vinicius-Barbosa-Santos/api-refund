c68a0cfd14927eb327698671cc4955546c035ba9
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-05-28 17:23:08 -0300

    chore: project structure

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
+import cors from "cors";
 import express from "express";
+import { errorHandling } from "./middlewares/error-handling";
+import { AppError } from "./utils/AppError";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Salvee!");
+  throw new AppError("Erro de Teste");
 });
+
+app.use(errorHandling);
 
 export { app };
