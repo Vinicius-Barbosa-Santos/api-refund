6828092fd2e82739acc37db9842d6db33dfd2433
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-06-03 09:42:10 -0300

    feat: ensure-authenticated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,16 @@ import { Router } from "express";
 import { usersRoutes } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
 import { refundsRoutes } from "./refunds-routes";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
 const routes = Router();
 
+// Rotas Publicas
 routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionsRoutes);
+
+// Rotas Privadas
+routes.use(ensureAuthenticated);
 routes.use("/refunds", refundsRoutes);
 
 export { routes };
