dcb17c05af6808d0946fb63649c92f758ee54706
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-06-03 19:46:20 -0300

    feat: request data validation

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from "express";
+import * as z from "zod";
+
+const RefundsCategory = z.enum([
+  "food",
+  "others",
+  "services",
+  "transport",
+  "accommodation",
+]);
 
 class RefundsController {
   async create(req: Request, res: Response) {
-    res.json({ message: "ok" });
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, { message: "Nome é obrigatório" }),
+      category: RefundsCategory,
+      amount: z.number().positive(),
+      filename: z.string().min(20),
+    });
+
+    const { name, category, amount, filename } = bodySchema.parse(req.body);
+
+    res.status(201).json({ name, category, amount, filename });
   }
 }
 
