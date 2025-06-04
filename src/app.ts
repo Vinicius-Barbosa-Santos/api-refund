3ea75b257828ccf551d0292ea9858f6ebfa0989f
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-06-04 15:06:01 -0300

    feat: route to display file

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import cors from "cors";
 import express from "express";
 import { routes } from "./routes";
 import { errorHandling } from "./middlewares/error-handling";
+import uploadConfig from "./config/upload";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 app.use(errorHandling);
